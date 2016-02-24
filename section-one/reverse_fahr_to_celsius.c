32a6f3de3c459cad409da11bd7b8f4548f40d721
Loraine <loraine@lorainekv.com>
2016-02-24 08:23:44 -0800

    Using symbolic constants.

--- section-one/reverse_fahr_to_celsius.c
+++ section-one/reverse_fahr_to_celsius.c
@@ -1,11 +1,17 @@
 # include <stdio.h>
 
+/* Symbolic constants */ 
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20 
+
 /* print Fahrenheit-Celsius table */ 
 main()
 {
     int fahr;
     
     printf("Fahr Cel\n");
-    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
