eff117e3a52133a84dc73255da128a3daa77ccf8
Loraine <loraine@lorainekv.com>
2016-02-27 10:35:06 -0800

    Working character count and test file.

--- section-one/count_chars.c
+++ section-one/count_chars.c
@@ -4,10 +4,9 @@
 
 main()
 {
-    long nc;
+    double nc;
 
-    nc = 0;
-    while (getchar() != EOF)
-        ++nc;
-    printf("%ld\n");
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
 }
