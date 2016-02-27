7c63261d389052c65426e4937df60914e47e2888
Loraine <loraine@lorainekv.com>
2016-02-27 10:37:05 -0800

    Commented learnings.

--- section-one/count_chars.c
+++ section-one/count_chars.c
@@ -5,8 +5,9 @@
 main()
 {
     double nc;
-
+        
+    /* you can use ++ as a prefix or postfix */ 
     for (nc = 0; getchar() != EOF; ++nc)
-        ;
+        ; /* lonely little null statement */ 
     printf("%.0f\n", nc);
 }
